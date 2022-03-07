a62bc005c41c0a02daabcfd5f88509f818a73c8c
Adrián Curiel <81223357+havc-dev@users.noreply.github.com>
2022-03-06 19:23:04 -0500

    Create Footer.jsx

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Footer = styled.footer`
+	width: 100%;
+	background-color: ${({ theme }) => theme.colors.bgContrast};
+	padding: ${({ theme }) => theme.padding.pV};
+	> p {
+		color: ${({ theme }) => theme.colors.primary};
+		text-align: center;
+		width: 95%;
+		margin: 0 auto;
+	}
+`;
