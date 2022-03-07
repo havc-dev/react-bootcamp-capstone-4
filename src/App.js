43f52ce446a96af7a1c25d660fb21cafce05cbde
Adrián Curiel <81223357+havc-dev@users.noreply.github.com>
2022-03-06 19:24:48 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
-import { useFeaturedBanners } from './utils/hooks/useFeaturedBanners';
+import { useState } from "react";
+
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme, GlobalStyles } from "./themes.js";
+
+import HomePage from "./pages/HomePage.jsx";
+import Header from "./components/header/Header";
+import { Footer } from "./components/footer/Footer.jsx";
+
+const StyledApp = styled.div`
+	color: ${(props) => props.theme.colors.text};
+	background-color: ${(props) => props.theme.colors.bg};
+	font-family: ${(props) => props.theme.fonts.family};
+	min-height: 100vh;
+	display: flex;
+	flex-direction: column;
+	justify-content: space-between;
+`;
 
 function App() {
-  const { data, isLoading } = useFeaturedBanners();
-  console.log(data, isLoading);
+	const date = new Date();
+	const year = date.getFullYear().toLocaleString();
+	const [theme, setTheme] = useState("light");
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const themeToggler = () => {
+		theme === "light" ? setTheme("dark") : setTheme("light");
+	};
+
+	return (
+		<ThemeProvider theme={theme === "dark" ? darkTheme : lightTheme}>
+			<GlobalStyles />
+			<StyledApp>
+				<Header themePicker={themeToggler} />
+				<HomePage />
+				<Footer>
+					<p>
+						{`Ecommerce created during Wizeline’s Academy React Bootcamp ${year}`}{" "}
+						&copy;
+					</p>
+				</Footer>
+			</StyledApp>
+		</ThemeProvider>
+	);
 }
 
 export default App;
