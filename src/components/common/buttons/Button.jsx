8b0502052f203e7870aabdae447bbd802356ab5e
Adrián Curiel <81223357+havc-dev@users.noreply.github.com>
2022-03-09 15:32:56 -0500

    Requirement 1

--- src/components/common/buttons/Button.jsx
+++ src/components/common/buttons/Button.jsx
@@ -1,10 +1,12 @@
 import styled from 'styled-components';
 
 export const Button = styled.button`
+	display: flex;
 	font-size: 1rem;
-	margin: ${({ theme }) => theme.spacing.s} auto;
+	background-color: ${({ theme }) => theme.colors.primary};
+	margin: ${({ theme }) => theme.spacing.sLg} auto;
 	padding: ${({ theme }) => theme.padding.p13};
 	border-radius: ${({ theme }) => theme.spacing.sSm};
-	color: ${({ theme }) => theme.colors.primary};
+	color: ${({ theme }) => theme.colors.bg};
 	border: 2px solidi ${(props) => props.theme.bg};
 `;
---
+++ src/pages/ProductListPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Page } from './Page';
+
+const ProductListPage = () => {
+	return (
+		<Page>
+			<h1>This is the Product List Page</h1>
+		</Page>
+	);
+};
+
+export default ProductListPage;
